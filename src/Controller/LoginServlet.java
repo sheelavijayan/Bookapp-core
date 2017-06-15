a9ea14648845237039e27b13a561602c6b8cb989
sheelavijayan <sheela9397@gmail.com>
2017-06-15 11:29:50 +0530

    first commit

---
+++ src/Controller/LoginServlet.java
@@ -0,0 +1,57 @@
+package Controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sheela.user.User;
+import com.sheela.userdao.UserDAO;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		out.println("logiin Servlet");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		out.println("Email=" + email);
+		out.println("password=" + password);
+		
+		User user=null;
+
+		UserDAO userdao = new UserDAO();
+		try {
+			user=userdao.login(email,password);
+			out.println( user);
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if(user==null)
+		{
+			response.sendRedirect("login.html");
+		}
+		else
+		{
+			response.sendRedirect("listBook.html");
+		}
+	}
+
+}
---
+++ src/com/sheela/testbookdao/TestBookDAO.java
@@ -0,0 +1,25 @@
+package com.sheela.testbookdao;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+import com.sheela.book.Book;
+import com.sheela.bookdao.BookDAO;
+import com.sheela.order.Order;
+import com.sheela.user.User;
+
+public class TestBookDAO {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		LocalDate date = LocalDate.parse("2016-12-12");
+		Book book = new Book();
+		book.setName("c++");
+		book.setPrice(100);
+		book.setPublished_date(date);
+
+		BookDAO bookdao = new BookDAO();
+		//bookdao.register(book);
+		System.out.println(book);
+			}
+	}
